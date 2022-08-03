433c5d02f115ac3dcd3e9f697fc309af2d648f5d
conho-web <conho@internet.ru>
2022-08-03 15:41:40 +0300

    V-1: created the project structure

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss"></style>
